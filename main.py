e45acd4ac992ebb05da182fb06b9167e8697cfd1
LodGvedeon <drovosekov1999@mail.ru>
2022-04-10 14:21:35 +0300

    :zap: My First Commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from pydantic.main import BaseModel
+import requests
+import pydantic
+
+
+class Post(BaseModel):
+    """
+    Base Post Model
+    """
+    userId: int
+    id: int
+    title: str
+    body: str
+    
+    def print_post_body(self):
+        print(self.body)
+    
+    def print_info(self):
+
+        print('User Id is: ', self.userId)
+        print('Post Id is: ', self.id)
+        print('Post Title is: ', self.title)
+        print('Post Body is: ', self.body)
+            
+print('Script starting')
+resp = requests.get(
+    'https://jsonplaceholder.typicode.com/posts/1'
+)
+data_raw = resp.json()
+post_new = Post(**data_raw)
+post_new.print_info()
+
